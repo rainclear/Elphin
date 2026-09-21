a45abde9223253a485d2867b339ff1d61309239e
Brian Zhang <zhang.haoyu@gmail.com>
2026-09-21 19:28:07 +0000

    feat: update thread pool to C++20. removed thread_pool.cpp

--- include/elphin/thread_pool.hpp
+++ include/elphin/thread_pool.hpp
@@ -6,54 +6,108 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <functional>
 #include <future>
-#include <memory>
+#include <functional>
+#include <type_traits>
+#include <utility>
 #include <stdexcept>
+#include <memory>
 
 namespace elphin::concurrent {
 
 class ThreadPool {
 public:
-    explicit ThreadPool(size_t threads = std::thread::hardware_concurrency());
-    ~ThreadPool();
+    explicit ThreadPool(size_t threads = std::thread::hardware_concurrency()) {
+        workers_.reserve(threads);
+        for (size_t i = 0; i < threads; ++i) {
+            workers_.emplace_back([this](std::stop_token stop_tok) {
+                worker_loop(stop_tok);
+            });
+        }
+    }
 
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::invoke_result<F, Args...>::type>;
-
+    // Non-copyable and non-movable
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(ThreadPool&&) = delete;
+    ThreadPool& operator=(ThreadPool&&) = delete;
+
+    ~ThreadPool() {
+        stop();
+    }
+
+    // Explicitly stop all workers and wake up waiting threads
+    void stop() {
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex_);
+            if (stopping_) {
+                return;
+            }
+            stopping_ = true;
+        }
+        
+        // Request stop across all std::jthreads
+        for (auto& worker : workers_) {
+            worker.request_stop();
+        }
+        
+        cv_.notify_all();
+    }
+
+    // Enqueue a task for execution using std::invoke_result_t (C++17/C++20)
+    template <typename F, typename... Args>
+    auto enqueue(F&& f, Args&&... args) 
+        -> std::future<std::invoke_result_t<F, Args...>> {
+        
+        using return_type = std::invoke_result_t<F, Args...>;
+
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        
+        std::future<return_type> res = task->get_future();
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex_);
+
+            if (stopping_) {
+                throw std::runtime_error("enqueue called on stopped ThreadPool");
+            }
+
+            tasks_.emplace([task]() { (*task)(); });
+        }
+        
+        cv_.notify_one();
+        return res;
+    }
 
 private:
-    std::vector<std::thread> workers_;
+    void worker_loop(std::stop_token stop_tok) {
+        while (true) {
+            std::function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex_);
+                cv_.wait(lock, [this, &stop_tok] {
+                    return stopping_ || stop_tok.stop_requested() || !tasks_.empty();
+                });
+
+                if ((stopping_ || stop_tok.stop_requested()) && tasks_.empty()) {
+                    return;
+                }
+
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+
+            task();
+        }
+    }
+
+    std::vector<std::jthread> workers_;
     std::queue<std::function<void()>> tasks_;
     std::mutex queue_mutex_;
     std::condition_variable cv_;
-    bool stop_{false};
+    bool stopping_{false};
 };
-
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::invoke_result<F, Args...>::type> {
-    
-    using return_type = typename std::invoke_result<F, Args...>::type;
-
-    auto task = std::make_shared<std::packaged_task<return_type()>>(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-    );
-    
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex_);
-        if (stop_) {
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-        }
-        tasks_.emplace([task]() { (*task)(); });
-    }
-    cv_.notify_one();
-    return res;
-}
 
 } // namespace elphin::concurrent
 
